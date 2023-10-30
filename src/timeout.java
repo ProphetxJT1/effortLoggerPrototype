41dd2cb85b9bfbae6597f2163304a88ede8576a1
ProphetxJT1 <138404706+ProphetxJT1@users.noreply.github.com>
2023-10-29 19:46:11 -0700

    Create timeout.java

---
+++ src/timeout.java
@@ -0,0 +1,62 @@
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import javafx.event.*;
+
+
+
+public class timeout extends VBox {
+	private Timeline time;
+	public timeout(Stage primaryStage) {
+		time = new Timeline(new KeyFrame(Duration.seconds(10), new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                Alert exit = new Alert(Alert.AlertType.INFORMATION);
+                exit.setHeaderText("You were inactive for (10) seconds, returning to loginpage");
+                exit.setTitle("Please relogin");
+                exit.show();
+
+                	primaryStage.close();
+                    openLoginPage();
+                    
+                    
+                    time.stop();
+                
+            }
+        	}));
+		
+			time.setCycleCount(Timeline.INDEFINITE);
+	        time.play();
+	
+	        primaryStage.addEventFilter(KeyEvent.ANY, new EventHandler<Event>() {
+	            @Override
+	            public void handle(Event event) {
+	                time.playFromStart();
+	            }
+	        });
+	        primaryStage.addEventFilter(MouseEvent.ANY, new EventHandler<Event>() {
+	            @Override
+	            public void handle(Event event) {
+	                time.playFromStart();
+	            }
+	        });
+	}
+
+
+
+	private void openLoginPage() {
+	    Stage loginStage = new Stage();
+	    LoginPage loginPage = new LoginPage(loginStage);
+	
+	    Scene loginScene = new Scene(loginPage, 400, 200);
+	    loginStage.setScene(loginScene);
+	
+	    loginStage.show();
+	}
+}
